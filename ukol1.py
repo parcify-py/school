f357337f8b77333a59610d4190eb9e0efee44ba1
unknown <makar.simonov@infis.cz>
2025-03-18 09:34:39 +0100

    commit

---
+++ ukol1.py
@@ -0,0 +1,3 @@
+# age = input('How old you')
+# name = input('What is your name')
+# print(f"Hello my {name} and my age is {age}")
---
+++ ukol2.py
@@ -0,0 +1,5 @@
+# first_number = float(input("Enter first number:"))
+# second_number = float(input("Enter second number:"))
+# print(f"{(first_number*second_number):.2f} {(first_number/second_number):.2f}{(first_number-second_number):.2f} {(first_number+second_number):.2f}")
+
+
